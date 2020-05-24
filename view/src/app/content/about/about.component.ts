29f4b86bfed33c1167f1711cfa306e9123e0948c
zuiwuchang <zuiwuchang@gmail.com>
2020-05-24 14:58:40 +0800

    about

--- view/src/app/content/about/about.component.ts
+++ view/src/app/content/about/about.component.ts
@@ -1,5 +1,11 @@
 import { Component, OnInit, VERSION } from '@angular/core';
-
+import { ServerAPI } from 'src/app/core/core/api';
+import { HttpClient } from '@angular/common/http';
+interface Version {
+  tag: string
+  commit: string
+  date: string
+}
 @Component({
   selector: 'app-about',
   templateUrl: './about.component.html',
@@ -7,9 +13,14 @@ import { Component, OnInit, VERSION } from '@angular/core';
 })
 export class AboutComponent implements OnInit {
   VERSION = VERSION
-  constructor() { }
+  version: Version
+  constructor(private httpClient: HttpClient,
+  ) { }
 
   ngOnInit(): void {
+    ServerAPI.v1.version.get<Version>(this.httpClient).then((data) => {
+      this.version = data
+    })
   }
 
 }
--- view/src/app/content/content.module.ts
+++ view/src/app/content/content.module.ts
@@ -1,5 +1,8 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { MatCardModule } from '@angular/material/card';
+import { MatListModule } from '@angular/material/list';
+import { MatIconModule } from '@angular/material/icon';
 
 import { ContentRoutingModule } from './content-routing.module';
 import { LicenseComponent } from './license/license.component';
@@ -10,6 +13,7 @@ import { AboutComponent } from './about/about.component';
   declarations: [LicenseComponent, AboutComponent],
   imports: [
     CommonModule,
+    MatCardModule, MatListModule, MatIconModule,
     ContentRoutingModule
   ]
 })
