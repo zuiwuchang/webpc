0a08e21aef414752d93a626da3d9bf51dab658c1
zuiwuchang <zuiwuchang@gmail.com>
2020-05-27 11:44:51 +0800

    audio video

--- view/src/app/fs/view/audio/audio.component.ts
+++ view/src/app/fs/view/audio/audio.component.ts
@@ -1,15 +1,60 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { SessionService } from 'src/app/core/session/session.service';
+import { ServerAPI } from 'src/app/core/core/api';
+import { isString } from 'util';
 
 @Component({
   selector: 'app-audio',
   templateUrl: './audio.component.html',
   styleUrls: ['./audio.component.scss']
 })
-export class AudioComponent implements OnInit {
-
-  constructor() { }
+export class AudioComponent implements OnInit, OnDestroy {
+  constructor(private router: Router,
+    private route: ActivatedRoute,
+    private sessionService: SessionService,
+  ) { }
 
   ngOnInit(): void {
+    this.sessionService.ready.then(() => {
+      if (this._closed) {
+        return
+      }
+      const param = this.route.snapshot.queryParamMap
+      const root = param.get(`root`)
+      const path = param.get(`path`)
+      this.root = root
+      this.filepath = path
+      const index = path.lastIndexOf('/')
+      if (index != -1) {
+        this.dir = path.substring(0, index)
+      }
+      this.url = ServerAPI.v1.fs.oneURL([root, path])
+      this.ready = true
+    })
   }
+  private _closed = false
+  ready: boolean
+  root: string
+  filepath: string
+  dir: string
+  url: string
+  ngOnDestroy() {
+    this._closed = true
+  }
+  onPathChange(path: string) {
+    if (!isString(path)) {
+      path = '/'
+    }
+    if (!path.startsWith('/')) {
+      path = '/' + path
+    }
 
+    this.router.navigate(['fs', 'list'], {
+      queryParams: {
+        root: this.root,
+        path: path,
+      }
+    })
+  }
 }
